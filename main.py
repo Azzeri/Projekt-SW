f5284e7d96515c12448d58f83da0b87e8b6d0622
Azzeri <artess2698@gmail.com>
2022-01-13 20:43:31 +0100

    buttons

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ ledGreen = LED(27)
 ledGreen.off()
 ledRed.off()
 
-button.when_pressed = ledGreen.on
-button.when_released = ledGreen.off
+buttonIncrease.when_pressed = ledGreen.on
+buttonIncrease.when_released = ledGreen.off
 
 vehicle = Vehicle()
 mainController = WaveController(50)
